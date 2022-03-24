129e1741f854d3724d74ce53ba89d289c3cc7e73
Chris Beer <chris@cbeer.info>
2022-03-24 08:44:27 -0700

    Work around an upstream view_component bug with inherited by ensuring compile is called at runtime

--- lib/blacklight/component.rb
+++ lib/blacklight/component.rb
@@ -2,5 +2,11 @@
 
 module Blacklight
   class Component < ViewComponent::Base
+    def render_in(view_context, &block)
+      # Work-around for https://github.com/github/view_component/issues/1315
+      self.class.compile(raise_errors: true) unless Rails.application.config.eager_load
+
+      super
+    end
   end
 end
